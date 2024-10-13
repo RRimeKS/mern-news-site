26775920c33486035acda7883fb9f4a802dcf0a5
Mehmet Emir TAŞKİN <mehmettaskin0@outlook.com>
2024-10-13 12:21:27 +0300

    Update newsApi.js

--- frontend/src/redux/api/newsApi.js
+++ frontend/src/redux/api/newsApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const newsApi = createApi({
   reducerPath: "newsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "/api2" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
   tagTypes: ["News"],
   endpoints: (builder) => ({
     getAllNews: builder.query({
