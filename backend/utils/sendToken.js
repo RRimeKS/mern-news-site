d0babab6ddb9133495b9b8729ba6955bfa0b0d3f
Mehmet Emir TAŞKİN <mehmettaskin0@outlook.com>
2024-10-13 17:16:58 +0300

    Update sendToken.js

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -2,7 +2,7 @@ exports.sendToken = async (user, res, statusCode) => {
   const token = await user.getJWToken();
 
   const cookieOpt = {
-    httpOnly: false,
+    httpOnly: true,
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000 * 7),
     secure: true,   // HTTPS üzerinden iletimi zorunlu kılar
     sameSite: 'None', // Cross-site cookie'leri etkinleştirir
