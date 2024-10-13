98013a6a1cc73d64d57c40a87ea66e637750cdb7
Mehmet Emir TAŞKİN <mehmettaskin0@outlook.com>
2024-10-13 11:27:10 +0300

    update

--- frontend/src/redux/api/newsApi.js
+++ frontend/src/redux/api/newsApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const newsApi = createApi({
   reducerPath: "newsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://mern-news-site-2.onrender.com/api" }),
   tagTypes: ["News"],
   endpoints: (builder) => ({
     getAllNews: builder.query({
