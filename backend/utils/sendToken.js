5c235dda6293f2ecfab9a0537cfed7c0c3faea5a
Mehmet Emir TAŞKİN <mehmettaskin0@outlook.com>
2024-10-13 17:14:23 +0300

    Update sendToken.js

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -2,10 +2,11 @@ exports.sendToken = async (user, res, statusCode) => {
   const token = await user.getJWToken();
 
   const cookieOpt = {
-    httpOnly: true,
+    httpOnly: false,
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000 * 7),
-    secure: false,   // HTTPS üzerinden iletimi zorunlu kılar
-    sameSite: 'None' // Cross-site cookie'leri etkinleştirir
+    secure: true,   // HTTPS üzerinden iletimi zorunlu kılar
+    sameSite: 'None', // Cross-site cookie'leri etkinleştirir
+    domain: "https://turanocaklarii.netlify.app"
   };
 
   res.status(statusCode).cookie("token", token, cookieOpt).json({ token });
