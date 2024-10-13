49e71fbf99486e3f38e0a13f019f4368580d25b8
Mehmet Emir TAŞKİN <mehmettaskin0@outlook.com>
2024-10-13 12:16:15 +0300

    Update categoryApi.js

--- frontend/src/redux/api/categoryApi.js
+++ frontend/src/redux/api/categoryApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const categoryApi = createApi({
   reducerPath: "categoryApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "/api" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://mern-news-site-2.onrender.com" }),
   tagTypes: ["Category"],
   endpoints: (builder) => ({
     getAllCategories: builder.query({
@@ -12,7 +12,7 @@ export const categoryApi = createApi({
     createCategory: builder.mutation({
       query(body) {
         return {
-          url: `/category`,
+          url: `/api/category`,
           method: "POST",
           body,
         };
