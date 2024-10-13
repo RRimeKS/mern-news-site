216a25a231f1bdb44eb651504ac622d0b26f6850
Mehmet Emir TAŞKİN <mehmettaskin0@outlook.com>
2024-10-13 16:30:30 +0300

    d

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -2,10 +2,12 @@ exports.sendToken = async (user, res, statusCode) => {
   const token = await user.getJWToken();
 
   const cookieOpt = {
-    httpOnly: true,
+    // httpOnly: true,
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000 * 7),
-    secure: false,   // HTTPS üzerinden iletimi zorunlu kılar
-    sameSite: 'None' // Cross-site cookie'leri etkinleştirir
+    // secure: false,   // HTTPS üzerinden iletimi zorunlu kılar
+    sameSite: 'strict', // Cross-site cookie'leri etkinleştirir
+    path: '/',
+    domain: 'https://turanocaklarii.netlify.app', // Eğer alt domain kullanıyorsanız doğru yapılandırın
   };
 
   res.status(statusCode).cookie("token", token, cookieOpt).json({ token });
